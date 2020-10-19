72e157ed0116f118ae5a6bb2831eea9f6c45488c
Jay Patel <pateljaypj1397@gmail.com>
2020-10-19 19:44:08 -0400

    User Registration

---
+++ graphql/resolvers/index.js
@@ -0,0 +1,11 @@
+const postsResolver = require('./posts');
+const usersResolver = require('./users');
+
+module.exports = {
+    Query: {
+        ...postsResolver.Query
+    },
+    Mutation: {
+        ...usersResolver.Mutation
+    }
+}
