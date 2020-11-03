12db8108f6fa828b88e26ae3b0272be9977238fc
Jay Patel <pateljaypj1397@gmail.com>
2020-11-03 16:53:36 -0500

    Routes and semantic-ui setup

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,4 @@
 import ReactDOM from 'react-dom';
-import reportWebVitals from './reportWebVitals';
 
 import ApolloProvider from './ApolloProvider';
 
@@ -8,4 +7,3 @@ ReactDOM.render(ApolloProvider, document.getElementById('root'));
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
